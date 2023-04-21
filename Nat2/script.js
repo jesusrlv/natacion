f285bad2f248d42a6fc2be3df91cfbc95988ae82
jersusrlv <jesusrlvrojo@gmail.com>
2023-04-21 01:10:45 -0600

    seccion

--- Nat2/script.js
+++ Nat2/script.js
@@ -4,10 +4,16 @@ $(function(){
         $('.nav-link').addClass("azul");
         $('.navscroll').addClass("azul");
         $('.navbar-brand').addClass("azul");
+        $('#navUno').attr("hidden",true);
+        $('#navDos').attr("hidden",false);
+        // $('.navscroll').addClass("text-center");
+        // $('.navscroll').addClass("text-light");
        } else {
         $(".nav-link").removeClass("azul");
         $(".navscroll").removeClass("azul");
         $(".navbar-brand").removeClass("azul");
+        $('#navUno').attr("hidden",false);
+        $('#navDos').attr("hidden",true);
        }
     });
 });
